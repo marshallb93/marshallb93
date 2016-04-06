1efc9a87e507f5105fd17a5352ee12d8f99d082f
Marshall Bradley <marshallbradley93@gmail.com>
2016-04-06 15:43:24 +0200

    Added multiple source files to display

--- wsgi/personalsite/pages/static/js/homescript.js
+++ wsgi/personalsite/pages/static/js/homescript.js
@@ -1,3 +1,6 @@
+var showing = false;
+var showingFunc;
+
 var showText = function (target, message, index, interval) {
     if (index < message.length) {
         if (message[index] == "\n") {
@@ -9,7 +12,10 @@ var showText = function (target, message, index, interval) {
         } else {
             $(target).append(message[index++]);
         }
-        setTimeout(showText, interval, target, message, index, interval);
+        showingFunc = setTimeout(showText, interval, target, message, index, interval);
+    } else {
+        showing = false;
+        clearTimeout(showingFunc);
     }
 }
 
@@ -19,8 +25,23 @@ var displayFile = function (file) {
     });
 }
 
-window.onload = function() {
+var pickRandomFile = function () {
+    if (!showing) {
+        showing = true;
+        var files = ["https://raw.githubusercontent.com/marshallb93/ds_coursework/master/src/main/java/Network.java",
+                     "https://raw.githubusercontent.com/marshallb93/pa_coursework/master/src/main/java/SimpleCache.java",
+                     "https://raw.githubusercontent.com/marshallb93/ca_coursework/master/Part%202/src/ca_coursework.py",
+                     "https://raw.githubusercontent.com/marshallb93/comn_coursework/master/COMN%20Part%202/src/Receiver2b.java",
+                     "https://raw.githubusercontent.com/SLIP-Group-B-2015/Raspberry_Python/master/src/main.py",
+                     "https://raw.githubusercontent.com/marshallb93/personal_site/master/wsgi/personalsite/pages/templates/main_index.html",
+                     "https://raw.githubusercontent.com/marshallb93/personal_site/master/wsgi/personalsite/pages/static/scss/styles.scss",
+                     "https://raw.githubusercontent.com/marshallb93/es_coursework/master/working/main.c"];
+        var random =  Math.floor(Math.random() * files.length);
+        displayFile(files[random]);
+    }
+    setTimeout(pickRandomFile, 50);
+}
 
-    var file = "https://raw.githubusercontent.com/marshallb93/ds_coursework/master/src/main/java/Network.java";
-    displayFile(file);
+window.onload = function() {
+    setTimeout(pickRandomFile, 50);
 };
